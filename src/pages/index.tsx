89a501b9bae58e69f20d08b923db1e69f9426909
HaQi <luqmanananta745@gmail.com>
2026-02-25 14:23:13 +0700

    feat: remove navbar from home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,6 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h1>Praktikum NextJS Pages Router</h1> <br />
       <p>Mahasiswa D-IV Teknik Informatika</p>
 
