d93e94dac4265f90c045e6be92779fc4dd37488e
HaQi <luqmanananta745@gmail.com>
2026-03-04 13:42:05 +0700

    refactor: revert slug rendering logic

--- src/pages/shop/[...slug].tsx
+++ src/pages/shop/[...slug].tsx
@@ -7,10 +7,8 @@ const halamanToko = () => {
     return (
         <div>
             <h1>Halaman toko</h1>
-            {/* <p>Toko: {`${query.slug && query.slug[0] + "-" + query.slug[1]}`}</p>
-             */}
-
-             <p>Toko: {Array.isArray(query.slug) ? query.slug.join("-") : query.slug}</p>
+            <p>Toko: {`${query.slug && query.slug[0] + "-" + query.slug[1]}`}</p>
+            {/* <p>Toko: {Array.isArray(query.slug) ? query.slug.join("-") : query.slug}</p> */}
         </div>
     );
 };
