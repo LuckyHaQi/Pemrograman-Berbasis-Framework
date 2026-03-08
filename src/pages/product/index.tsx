1feb0d6e2a1c875c85f94dd89b41dcee3cd2a3f3
HaQi <luqmanananta745@gmail.com>
2026-03-09 00:10:55 +0700

    feat: add logout functionality to products page

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -9,9 +9,19 @@ const ProductsPage = () => {
             push("/auth/login");
         }
     },[]);
+
+    const handleLogout = () => {
+        localStorage.removeItem("isLogin");
+        push("/auth/login");
+    };
+
     return (
         <div>
             Product Page
+
+            <button onClick={handleLogout}>
+                Logout
+            </button>
         </div>
     );
 };
